c990f60084ad7beecc2fed25421beff81cb288a4
Daichi Katayama <daipanchi@gmail.com>
2021-12-13 19:40:25 +0900

    sanitize code

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,12 @@
 class Note < ActiveRecord::Base
-  scope :keystrokes, -> { where.not(event: 176).where.not(velocity: 0) }
+  scope :keystrokes, -> {
+    where.not(event: 176).
+    where.not(velocity: 0)
+  }
 
   def self.count_of_today
-    Note.where(event: 144, created_at: Time.now.all_day).where.not(velocity: 0).count
+    Note.where(event: 144, created_at: Time.now.all_day)
+        .where.not(velocity: 0)
+        .count
   end
 end
--- console.rb
+++ console.rb
@@ -1,15 +1,9 @@
-require 'yaml'
-require 'active_record'
-require 'pry'
-require 'pry-byebug'
+require "yaml"
+require "active_record"
 
-dbconfig = YAML.load(File.read('config/database.yml'))
-ActiveRecord::Base.establish_connection dbconfig['db']
-require 'groupdate'
-require './app/models/note.rb'
+dbconfig = YAML.load(File.read("config/database.yml"))
+ActiveRecord::Base.establish_connection dbconfig["db"]
+require "groupdate"
+require "./app/models/note.rb"
 
 p Note.group_by_day(:created_at).keystrokes.count
-
-binding.pry
-
-Note.group_by_day(:created_at).keystrokes.count
